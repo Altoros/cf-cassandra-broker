1852f966705104722637fd96d80118c6b9bbd9d0
Maksim Zhylinski <uzzable@gmail.com>
2015-04-17 11:37:48 +0300

    Tests for random

---
+++ random/random_test.go
@@ -0,0 +1,22 @@
+package random_test
+
+import (
+	"github.com/Altoros/cf-cassandra-service-broker/random"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("random", func() {
+	Describe(".Bytes", func() {
+		It("returns bytes array of given lenght", func() {
+			Ω(len(random.Bytes(10))).To(Equal(10))
+		})
+	})
+
+	Describe(".Hex", func() {
+		It("returns string of 2x length", func() {
+			Ω(len(random.Hex(10))).To(Equal(20))
+		})
+	})
+})
